5e455dbbbbe432e26ae4487b9d9b5c3fb9d62701
Renkin42 <blueboysw@gmail.com>
2014-02-03 21:50:36 -0700

    added entity tomahawk

---
+++ src/main/java/renkin42/stuffWorthThrowing/entities/EntityTomahawk.java
@@ -0,0 +1,39 @@
+package renkin42.stuffWorthThrowing.entities;
+
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.projectile.EntityThrowable;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.world.World;
+
+public class EntityTomahawk extends EntityThrowable {
+
+	private byte strength;
+	public String tomahawkName;
+	
+	public EntityTomahawk(World par1World) {
+		super(par1World);
+	}
+
+	public EntityTomahawk(World par1World, EntityLivingBase par2EntityLivingBase) {
+		super(par1World, par2EntityLivingBase);
+	}
+	
+	public EntityTomahawk(World par1World, EntityLivingBase par2EntityLivingBase, byte strength, String tomahawkName) {
+		super(par1World, par2EntityLivingBase);
+		this.strength = strength;
+		this.tomahawkName = tomahawkName;
+	}
+
+	public EntityTomahawk(World par1World, double par2, double par4, double par6) {
+		super(par1World, par2, par4, par6);
+	}
+
+	@Override
+	protected void onImpact(MovingObjectPosition par1MovingObjectPosition) {
+		if (par1MovingObjectPosition.entityHit != null) {
+			par1MovingObjectPosition.entityHit.attackEntityFrom(DamageSource.causeThrownDamage(this, this.getThrower()), this.strength);
+		}
+	}
+
+}
--- src/main/java/renkin42/stuffWorthThrowing/items/ItemTomahawk.java
+++ src/main/java/renkin42/stuffWorthThrowing/items/ItemTomahawk.java
@@ -1,6 +1,7 @@
 package renkin42.stuffWorthThrowing.items;
 
 import renkin42.stuffWorthThrowing.StuffWorthThrowing;
+import renkin42.stuffWorthThrowing.entities.EntityTomahawk;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
@@ -9,21 +10,29 @@ import net.minecraft.world.World;
 
 public class ItemTomahawk extends Item {
 	
-	public ItemTomahawk(String unlocalizedName) {
+	private byte strength;
+	private String tomahawkName;
+	
+	public ItemTomahawk(String unlocalizedName, byte strength) {
 		setMaxStackSize(4);
 		setCreativeTab(CreativeTabs.tabTools);
 		setUnlocalizedName(unlocalizedName);
 		setTextureName(StuffWorthThrowing.mod_id + ":" + unlocalizedName);
+		this.strength = strength;
+		this.tomahawkName = unlocalizedName;
 	}
 	
 	@Override
 	public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer) {
-		if (!par3EntityPlayer.capabilities.isCreativeMode)
-        {
+		if (!par3EntityPlayer.capabilities.isCreativeMode) {
             --par1ItemStack.stackSize;
         }
 		
 		par2World.playSoundAtEntity(par3EntityPlayer, "random.bow", 0.5F, 0.4F / (itemRand.nextFloat() * 0.4F + 0.8F));
+		
+		if (!par2World.isRemote) {
+			par2World.spawnEntityInWorld(new EntityTomahawk(par2World, par3EntityPlayer, this.strength, this.tomahawkName));
+		}
 
 		return par1ItemStack;
         
