42f29d3ede45def94f77509b469402f28d390acd
Renkin42 <blueboysw@gmail.com>
2014-08-08 20:08:55 -0600

    Added breaking particles to tomahawk, not sure if it worked...

--- src/main/java/renkin42/stuffWorthThrowing/entities/EntityTomahawk.java
+++ src/main/java/renkin42/stuffWorthThrowing/entities/EntityTomahawk.java
@@ -6,6 +6,7 @@ import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntityThrowable;
 import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.MovingObjectPosition;
@@ -50,24 +51,35 @@ public class EntityTomahawk extends EntityThrowable {
 	    }
 		++this.damage; //Add 2 damage if an enemy is hit, 1 otherwise.
 		ItemStack tomahawk;
+		int tomahawkid;
 		if (this.tomahawkName.equals("tomahawkWood")) {
 			tomahawk = new ItemStack(StuffWorthThrowingItems.tomahawkWooden);
+			tomahawkid = Item.getIdFromItem(StuffWorthThrowingItems.tomahawkWooden);
 		} else if (this.tomahawkName.equals("tomahawkStone")) {
 			tomahawk = new ItemStack(StuffWorthThrowingItems.tomahawkStone);
+			tomahawkid = Item.getIdFromItem(StuffWorthThrowingItems.tomahawkStone);
 		} else if (this.tomahawkName.equals("tomahawkIron")) {
 			tomahawk = new ItemStack(StuffWorthThrowingItems.tomahawkIron);
+			tomahawkid = Item.getIdFromItem(StuffWorthThrowingItems.tomahawkIron);
 		} else if (this.tomahawkName.equals("tomahawkGold")) {
 			tomahawk = new ItemStack(StuffWorthThrowingItems.tomahawkGold);
+			tomahawkid = Item.getIdFromItem(StuffWorthThrowingItems.tomahawkGold);
 		} else if (this.tomahawkName.equals("tomahawkDiamond")) {
 			tomahawk = new ItemStack(StuffWorthThrowingItems.tomahawkDiamond);
+			tomahawkid = Item.getIdFromItem(StuffWorthThrowingItems.tomahawkDiamond);
 		} else {
 			tomahawk = new ItemStack(Items.wooden_axe); //Why not?
+			tomahawkid = Item.getIdFromItem(Items.wooden_axe);
 		}
 		tomahawk.setItemDamage(this.damage);
 		if (!this.worldObj.isRemote) {
 			if (!((EntityPlayer)this.getThrower()).capabilities.isCreativeMode) {
 				if (this.damage < tomahawk.getItem().getMaxDamage()) {
 					this.worldObj.spawnEntityInWorld(new EntityItem(this.worldObj, this.posX, this.posY, this.posZ, tomahawk));
+				} else {
+					for (int i = 0; i < 8; ++i) {
+						this.worldObj.spawnParticle("iconcrack_"+tomahawkid, this.posX, this.posY, this.posZ, 0.0D, 0.0D, 0.0D);
+				    }
 				}
 			}
 			this.setDead();
